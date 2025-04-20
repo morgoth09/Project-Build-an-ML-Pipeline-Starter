4a7f5a933087547b1460f254b32152f775b91407
root <root@Psychotron.>
2025-04-20 15:47:52 -0700

    Updated run.py

--- main.py
+++ main.py
@@ -54,19 +54,50 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _= mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "cleaned_data",
+                    "output_description": "Data with outliers and nulls removed",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+    )
 
         if "data_check" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            _= mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "asklar-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:latest",
+                    "ref": "asklar-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:latest",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+    },
+    )
 
         if "data_split" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters={
+                    "input": "asklar-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:latest",  # Input artifact
+                    "test_size": str(config["modeling"]["test_size"]),  # Fraction of data to use for test
+                    "random_seed": str(config["modeling"]["random_seed"]),  # Random seed for reproducibility
+                    "stratify_by": config["modeling"]["stratify_by"]  # Column to use for stratification (if any)
+                }
+    )
+
 
         if "train_random_forest" in active_steps:
 
@@ -77,12 +108,22 @@ def go(config: DictConfig):
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": str(config["modeling"]["val_size"]),
+                    "random_seed": str(config["modeling"]["random_seed"]),
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": str(config["modeling"]["max_tfidf_features"]),
+                    "output_artifact": "random_forest_export"
+                }
+            )
+    
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            
 
         if "test_regression_model" in active_steps:
 
@@ -90,7 +131,14 @@ def go(config: DictConfig):
             # Implement here #
             ##################
 
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                }
+            )
 
 
 if __name__ == "__main__":
